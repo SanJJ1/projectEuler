c86ab6ccd31bb5c4acebf6669eddc2cce39422fc
Sanjay Janardhan <21janardhansanjay@gmail.com>
2022-02-14 21:46:03 -0500

    adds pascal triangle

--- 15 - Lattice paths.py
+++ 15 - Lattice paths.py
@@ -4,18 +4,18 @@
 
 import sys
 
-print(sys.getrecursionlimit())
+# print(sys.getrecursionlimit())
 sys.setrecursionlimit(10000)
 
 x = {}
 # calls = {}
 
 
-def lattice(m, n):
-    if m == 1:
-        return n + 1
-    if n == 1:
-        return m + 1
+def lat(m, n):
+    if m == 0:
+        return 1
+    if n == 0:
+        return 1
     # print(m, n)
     # Slow:
     # return lattice(m - 1, n) + lattice(m, n - 1)
@@ -26,9 +26,14 @@ def lattice(m, n):
     # Since a 19x2 will have the same value as 2x19, we can take advantage of this:
     if (n, m) in x:
         return x[(n, m)]
-    x[(m, n)] = lattice(m - 1, n) + lattice(m, n - 1)
+    x[(m, n)] = lat(m - 1, n) + lat(m, n - 1)
     return x[(m, n)]
 
 
-print(lattice(1000, 1000))
-# print(x)
+print(lat(1200, 1100))
+# # # Prints Pascal's triangle using the lattice method
+# # [print(" ".join([str(lat(i - j, j)) for j in range(i + 1)])) for i in range(10)]
+# for i in range(41):
+#     for j in range(i + 1):
+#         print(lat(i - j, j), end =" ")
+#     print()
